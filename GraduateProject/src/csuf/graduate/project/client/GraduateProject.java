a46a387977542339d396bee9502106b163e617b4
James Cha <james.cha.zero@gmail.com>
2013-11-24 15:50:13 -0800

    Shifting Charts

--- GraduateProject/src/csuf/graduate/project/client/GraduateProject.java
+++ GraduateProject/src/csuf/graduate/project/client/GraduateProject.java
@@ -1,8 +1,5 @@
 package csuf.graduate.project.client;
 
-import org.moxieapps.gwt.highcharts.client.Chart;
-import org.moxieapps.gwt.highcharts.client.Series;
-
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.RootPanel;
@@ -14,14 +11,7 @@ public class GraduateProject implements EntryPoint {
 	/**
 	 * Create a remote service proxy to talk to the server-side Greeting service.
 	 */
-	Chart chart = new Chart()
-	   .setType(Series.Type.SPLINE)
-	   .setChartTitleText("Lawn Tunnels")
-	   .setMarginRight(10);
 	
-	Series series = chart.createSeries()
-			   .setName("Moles per Yard")
-			   .setPoints(new Number[] { 163, 203, 276, 408, 547, 729, 628 });
 	
 	private HorizontalPanel homePanel = new HorizontalPanel();
 	
@@ -35,8 +25,7 @@ public class GraduateProject implements EntryPoint {
 		homePanel.add(home);
 		RootPanel.get("home").add(homePanel);
 		
-		chart.addSeries(series);
-		RootPanel.get().add(chart);
+		
 		
 	}
 }
--- GraduateProject/src/csuf/graduate/project/shared/Listen.java
+++ GraduateProject/src/csuf/graduate/project/shared/Listen.java
@@ -1,21 +1,41 @@
 package csuf.graduate.project.shared;
 
 import java.io.IOException;
+import java.util.Date;
 
 import net.tinyos.packet.BuildSource;
 import net.tinyos.packet.PacketSource;
 import net.tinyos.util.Dump;
 import net.tinyos.util.PrintStreamMessenger;
 
+import org.moxieapps.gwt.highcharts.client.Chart;
+import org.moxieapps.gwt.highcharts.client.Series;
+
+import com.google.gwt.user.client.ui.RootPanel;
+
 public class Listen {
+	Chart chart = new Chart()
+	   .setType(Series.Type.SPLINE)
+	   .setChartTitleText("Lawn Tunnels")
+	   .setMarginRight(10);
+	
+	Series series = chart.createSeries()
+			   .setName("Moles per Yard")
+			   .setPoints(new Number[] { 163, 203, 276, 408, 547, 729, 628 });
+	
 	
 	public Listen (String source) {
         PacketSource reader = BuildSource.makePacketSource(source);
 		
+        chart.addSeries(series);
+		RootPanel.get().add(chart);
+        
 		try {
 			  reader.open(PrintStreamMessenger.err);
 			  for (;;) {
 			    byte[] packet = reader.readPacket();
+			    
+			    series.addPoint(new Date().getTime(), 10, true, true, true);
 			    Dump.printPacket(System.out, packet);
 			    System.out.println();
 			    System.out.flush();
