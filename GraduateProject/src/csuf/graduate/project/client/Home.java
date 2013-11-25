cf2437241c384818f0669c3a90475f883ab79a26
James Cha <james.cha.zero@gmail.com>
2013-11-25 01:23:56 -0800

    Added 2nd Node

--- GraduateProject/src/csuf/graduate/project/client/Home.java
+++ GraduateProject/src/csuf/graduate/project/client/Home.java
@@ -32,9 +32,16 @@ public class Home extends Composite {
 
 	private static HomeUiBinder uiBinder = GWT.create(HomeUiBinder.class);
 	
+	
+	//Node 1
 	Boolean mspToggle = true;
 	Boolean senToggle = true;
 	Boolean humToggle = true;
+	
+	//Node 2
+	Boolean mspToggle2 = true;
+	Boolean senToggle2 = true;
+	Boolean humToggle2 = true;
 	
 	private final GreetingServiceAsync greetingService = GWT
 			.create(GreetingService.class);
@@ -66,13 +73,22 @@ public class Home extends Composite {
     );
 	
 	final Series msp = chart.createSeries()
-			.setName("MSP Temperature");
+			.setName("Node 1 MSP Temperature");
 	
 	final Series hum = chart.createSeries()
-			.setName("Humidity");
+			.setName("Node 1 Humidity");
 	
 	final Series sen = chart.createSeries()
-			.setName("Sen Temperature");
+			.setName("Node 1 Sen Temperature");
+	
+	final Series msp2 = chart.createSeries()
+			.setName("Node 2 MSP Temperature");
+	
+	final Series hum2 = chart.createSeries()
+			.setName("Node 2 Humidity");
+	
+	final Series sen2 = chart.createSeries()
+			.setName("Node 2 Sen Temperature");
 	
 
 	interface HomeUiBinder extends UiBinder<Widget, Home> {
@@ -97,6 +113,9 @@ public class Home extends Composite {
 		chart.addSeries(msp);
 		chart.addSeries(sen);
 		chart.addSeries(hum);
+		chart.addSeries(msp2);
+		chart.addSeries(sen2);
+		chart.addSeries(hum2);
 		RootPanel.get().add(chart);
 	}
 
@@ -105,7 +124,10 @@ public class Home extends Composite {
 	Button MSPButton;
 	Button SenButton;
 	Button HumButton;
-	Heading test;
+
+	Button MSPButton2;
+	Button SenButton2;
+	Button HumButton2;
 	
 	@UiHandler("MSPButton")
 	void MSP(ClickEvent e){
@@ -137,6 +159,37 @@ public class Home extends Composite {
 		humToggle = !humToggle;
 	}
 	
+	//Node 2
+	@UiHandler("MSPButton2")
+	void MSP2(ClickEvent e){
+		if (mspToggle2)
+			msp2.hide();
+		else
+			msp2.show();
+		
+		mspToggle2 = !mspToggle2;
+	}
+	
+	@UiHandler("SenButton2")
+	void Sen2(ClickEvent e){
+		if (senToggle2)
+			sen2.hide();
+		else
+			sen2.show();
+		
+		senToggle2 = !senToggle2;
+	}
+	
+	@UiHandler("HumButton2")
+	void Hum2(ClickEvent e) {
+		if (humToggle2)
+			hum2.hide();
+		else
+			hum2.show();
+		
+		humToggle2 = !humToggle2;
+	}
+	
 	
 
 	@UiHandler("startButton")
@@ -163,81 +216,176 @@ public class Home extends Composite {
 						Double temperatureSen = 0.0;
 						Double humidity = 0.0;
 						
+						//Very Dirty Way. Making a Class would be cleaner.
+						Integer countMSP2 = 0;
+						Integer countSen2 = 0;
+						Integer countHum2 = 0;
+						Integer sumMSP2 = 0;
+						Integer sumHum2 = 0;
+						Integer sumSen2 = 0;
+						Double averageMSP2 = 0.0;
+						Double averageHum2 = 0.0;
+						Double averageSen2 = 0.0;
+						Double temperatureMSP2 = 0.0;
+						Double temperatureSen2 = 0.0;
+						Double humidity2 = 0.0;
+						
+						System.out.println(result);
+						
 						final String[] newTextArray = result.split(" ", -1);
-						for(Integer i = 0; i<20; i+=2) {
-							System.out.println(result);
-							System.out.println(newTextArray[15]);
-							System.out.println(newTextArray[16]);
-							System.out.println(Integer.parseInt((newTextArray[16+i] + newTextArray[15+i]),16));
-							if(Integer.parseInt((newTextArray[16+i] + newTextArray[15+i]),16) < 1500 ) { //Humidity
-								sumHum += Integer.parseInt((newTextArray[16+i] + newTextArray[15+i]),16);
-								countHum++;
-							}
-							else if (Integer.parseInt((newTextArray[16+i] + newTextArray[15+i]),16) > 1500&& //MSP430 
-									 Integer.parseInt((newTextArray[16+i] + newTextArray[15+i]),16) < 3500 ) {
-								sumMSP += Integer.parseInt((newTextArray[16+i] + newTextArray[15+i]),16);
-								countMSP++;
-							}
-							else if (Integer.parseInt((newTextArray[16+i] + newTextArray[15+i]),16) > 5500) { //Sensirian
-								sumSen += Integer.parseInt((newTextArray[16+i] + newTextArray[15+i]),16);
-								countSen++;
+						if (newTextArray[13].toString().contentEquals("01")) {
+							for(Integer i = 0; i<20; i+=2) {
+								if(Integer.parseInt((newTextArray[16+i] + newTextArray[15+i]),16) < 1500 ) { //Humidity
+									sumHum += Integer.parseInt((newTextArray[16+i] + newTextArray[15+i]),16);
+									countHum++;
+								}
+								else if (Integer.parseInt((newTextArray[16+i] + newTextArray[15+i]),16) > 1500&& //MSP430 
+										 Integer.parseInt((newTextArray[16+i] + newTextArray[15+i]),16) < 3500 ) {
+									sumMSP += Integer.parseInt((newTextArray[16+i] + newTextArray[15+i]),16);
+									countMSP++;
+								}
+								else if (Integer.parseInt((newTextArray[16+i] + newTextArray[15+i]),16) > 5500) { //Sensirian
+									sumSen += Integer.parseInt((newTextArray[16+i] + newTextArray[15+i]),16);
+									countSen++;
+								}
+								
+								averageMSP = sumMSP/(countMSP * 1.0);
+								averageSen = sumSen/(countSen * 1.0);
+								averageHum = sumHum/(countHum * 1.0);
+								
+								temperatureMSP = ((((((averageMSP / 4096.0) * 1.5) - 0.986) / 0.00355) * 9.0) / 5.0) + 32.0;
+								temperatureSen = (((-38.4 + (averageSen * 0.0098)) * 9.0) / 5.0) + 32.0;
+								humidity = (-0.0000028 * averageHum * averageHum) + (0.0405 * averageHum - 4) ;
+								
 							}
 							
-							averageMSP = sumMSP/(countMSP * 1.0);
-							averageSen = sumSen/(countSen * 1.0);
-							averageHum = sumHum/(countHum * 1.0);
+							//Node 1
+							if (msp.getPoints().length > 15) {
+								msp.addPoint(  
+					                    new Date().getTime(),  
+					                    temperatureMSP,  
+					                    true, true, true  
+					                );
+							}
+							else {
+								msp.addPoint(  
+					                    new Date().getTime(),  
+					                    temperatureMSP,  
+					                    true, false, true  
+					                );
+							}
 							
-							temperatureMSP = ((((((averageMSP / 4096.0) * 1.5) - 0.986) / 0.00355) * 9.0) / 5.0) + 32.0;
-							temperatureSen = (((-38.4 + (averageSen * 0.0098)) * 9.0) / 5.0) + 32.0;
-							humidity = (-0.0000028 * averageHum * averageHum) + (0.0405 * averageHum - 4) ;
+							if (sen.getPoints().length > 15) {
+								sen.addPoint(  
+					                    new Date().getTime(),  
+					                    temperatureSen,  
+					                    true, true, true  
+					                );
+							}
+							else {
+								sen.addPoint(  
+					                    new Date().getTime(),  
+					                    temperatureSen,  
+					                    true, false, true  
+					                );
+							}
 							
+							
+							if (hum.getPoints().length > 15) {
+								hum.addPoint(  
+					                    new Date().getTime(),  
+					                    humidity,  
+					                    true, true, true  
+					                );
+							}
+							else {
+								hum.addPoint(  
+					                    new Date().getTime(),  
+					                    humidity,  
+					                    true, false, true  
+					                );
+							}
 						}
 						
-						if (msp.getPoints().length > 15) {
-							msp.addPoint(  
-				                    new Date().getTime(),  
-				                    temperatureMSP,  
-				                    true, true, true  
-				                );
-						}
-						else {
-							msp.addPoint(  
-				                    new Date().getTime(),  
-				                    temperatureMSP,  
-				                    true, false, true  
-				                );
+						if (newTextArray[13].toString().contentEquals("02")) {
+							for(Integer i = 0; i<20; i+=2) {
+								if(Integer.parseInt((newTextArray[16+i] + newTextArray[15+i]),16) < 1500 ) { //Humidity
+									sumHum2 += Integer.parseInt((newTextArray[16+i] + newTextArray[15+i]),16);
+									countHum2++;
+								}
+								else if (Integer.parseInt((newTextArray[16+i] + newTextArray[15+i]),16) > 1500&& //MSP430 
+										 Integer.parseInt((newTextArray[16+i] + newTextArray[15+i]),16) < 3500 ) {
+									sumMSP2 += Integer.parseInt((newTextArray[16+i] + newTextArray[15+i]),16);
+									countMSP2++;
+								}
+								else if (Integer.parseInt((newTextArray[16+i] + newTextArray[15+i]),16) > 5500) { //Sensirian
+									sumSen2 += Integer.parseInt((newTextArray[16+i] + newTextArray[15+i]),16);
+									countSen2++;
+								}
+								
+								averageMSP2 = sumMSP2/(countMSP2 * 1.0);
+								averageSen2 = sumSen2/(countSen2 * 1.0);
+								averageHum2 = sumHum2/(countHum2* 1.0);
+								
+								temperatureMSP2 = ((((((averageMSP2 / 4096.0) * 1.5) - 0.986) / 0.00355) * 9.0) / 5.0) + 32.0;
+								temperatureSen2 = (((-38.4 + (averageSen2 * 0.0098)) * 9.0) / 5.0) + 32.0;
+								humidity2 = (-0.0000028 * averageHum2 * averageHum2) + (0.0405 * averageHum2 - 4) ;
+							}
+							
+							//Node 2
+							if (msp2.getPoints().length > 15) {
+								msp2.addPoint(  
+					                    new Date().getTime(),  
+					                    temperatureMSP2,  
+					                    true, true, true  
+					                );
+							}
+							else {
+								msp2.addPoint(  
+					                    new Date().getTime(),  
+					                    temperatureMSP2,  
+					                    true, false, true  
+					                );
+							}
+							
+							if (sen2.getPoints().length > 15) {
+								sen2.addPoint(  
+					                    new Date().getTime(),  
+					                    temperatureSen2,  
+					                    true, true, true  
+					                );
+							}
+							else {
+								sen2.addPoint(  
+					                    new Date().getTime(),  
+					                    temperatureSen2,  
+					                    true, false, true  
+					                );
+							}
+							
+							
+							if (hum2.getPoints().length > 15) {
+								hum2.addPoint(  
+					                    new Date().getTime(),  
+					                    humidity2,  
+					                    true, true, true  
+					                );
+							}
+							else {
+								hum2.addPoint(  
+					                    new Date().getTime(),  
+					                    humidity2,  
+					                    true, false, true  
+					                );
+							}
 						}
 						
-						if (sen.getPoints().length > 15) {
-							sen.addPoint(  
-				                    new Date().getTime(),  
-				                    temperatureSen,  
-				                    true, true, true  
-				                );
-						}
-						else {
-							sen.addPoint(  
-				                    new Date().getTime(),  
-				                    temperatureSen,  
-				                    true, false, true  
-				                );
-						}
 						
 						
-						if (hum.getPoints().length > 15) {
-							hum.addPoint(  
-				                    new Date().getTime(),  
-				                    humidity,  
-				                    true, true, true  
-				                );
-						}
-						else {
-							hum.addPoint(  
-				                    new Date().getTime(),  
-				                    humidity,  
-				                    true, false, true  
-				                );
-						}
+						
+						
+						
+						
 							
 				}
 					
